735e138df66115521be6e5ac9497a5aa54a1f6f8
AastikRajan <aastikc15@gmail.com>
2025-02-19 01:18:41 +0530

    Remove nested submodule folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This allows production builds to succeed even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // Add any other config options here if needed.
 };
 
 export default nextConfig;
